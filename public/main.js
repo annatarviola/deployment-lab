dd41ae298bdee07e477af7d2f5c2fee56d3841e6
Lily Springer <lilyspringer@hotmail.com>
2023-03-29 20:46:53 -0600

    scam man added

---
+++ public/main.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("submit")
+const overlay = document.getElementById("overlay")
+
+btn.addEventListener("click", e => {
+    e.preventDefault()
+    overlay.style.display = "block";
+
+    const scamMan = document.createElement("img")
+    scamMan.src = "https://i.ibb.co/4jzL2kc/scamman-removebg-preview.png"
+    scamMan.id = "scamman"
+    document.body.appendChild(scamMan)
+
+    const alert = document.createElement("h1")
+    alert.textContent = "YOU HAVE BEEN SCAMMED"
+    alert.id = "alert"
+    document.body.appendChild(alert)
+})
